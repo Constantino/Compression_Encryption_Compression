3585bf275c95ff3f0668699fd1e06cd8db49f257
VictorRiosM <victor.riosm@live.com>
2015-10-25 16:09:07 -0600

    Reading text file

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2
+from sys import argv, exit
+
+if __name__ == 'main':
+	
+	try:
+		file_name = argv[1]
+	except:
+		exit("Couldn't read text file")
+	
+	dictionary = Dictionary(file_name)
