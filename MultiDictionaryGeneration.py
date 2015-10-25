997c420ccdc4566ce1abd1d3d870d1243e43c4d8
VictorRiosM <victor.riosm@live.com>
2015-10-25 15:21:08 -0600

    WordFrequencies

--- MultiDictionaryGeneration.py
+++ MultiDictionaryGeneration.py
@@ -4,3 +4,12 @@ class MultiDictionaryGeneration:
 		fileInput = open(file_name, 'r')
 		text = fileInput.read()
 		print text
+
+	def countWords(text):
+		for word in text:
+			frequency = frecuencies.get(word, None)
+            if frequency == None:
+               frequencies[word] = 1
+            else:
+               frequencies[word] = frequency + 1
+        return frequencies
