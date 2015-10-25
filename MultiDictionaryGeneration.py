5034b2d0cb032861c2d0426440bbb5eeae88a531
VictorRiosM <victor.riosm@live.com>
2015-10-25 15:28:10 -0600

    Correct_Identation_MultiDictionaryGeneration.py

--- MultiDictionaryGeneration.py
+++ MultiDictionaryGeneration.py
@@ -6,10 +6,10 @@ class MultiDictionaryGeneration:
 		print text
 
 	def countWords(text):
-		for word in text:
-			frequency = frecuencies.get(word, None)
-            if frequency == None:
-               frequencies[word] = 1
-            else:
-               frequencies[word] = frequency + 1
-        return frequencies
+	    for word in text:
+		frequency = frecuencies.get(word, None)
+        	if frequency == None:
+                    frequencies[word] = 1
+                else:
+                    frequencies[word] = frequency + 1
+            return frequencies
