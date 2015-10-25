640b81b205b937a9ad57b0a391cb2f0c562f2cde
VictorRiosM <victor.riosm@live.com>
2015-10-25 16:16:36 -0600

    Identation

--- Dictionary.py
+++ Dictionary.py
@@ -1,18 +1,18 @@
 class Dictionary:
 
-	def __init__(self, file_name):
-		self._filename = file_name
+    def __init__(self, file_name):
+	self._filename = file_name
 
-	def read_file(self):
-		fileInput = open(self._filename, 'r')
-		text = fileInput.read()
-		return text
+    def read_file(self):
+	fileInput = open(self._filename, 'r')
+	text = fileInput.read()
+	return text
 
-	def countWords(text):
-		for word in text:
-			frequency = frecuencies.get(word, None)
-			if frequency == None:
-				frequencies[word] = 1
-			else:
-				frequencies[word] = frequency + 1
-		return frequencies
+    def countWords(text):
+	for word in text:
+            frequency = frecuencies.get(word, None)
+	    if frequency == None:
+		frequencies[word] = 1
+	    else:
+		frequencies[word] = frequency + 1
+	return frequencies
