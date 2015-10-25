3c00709a6f84b01592b98b55721df20802b75b1f
VictorRiosM <victor.riosm@live.com>
2015-10-25 16:52:39 -0600

    Word count

--- Dictionary.py
+++ Dictionary.py
@@ -2,19 +2,16 @@ class Dictionary:
 
     def __init__(self, file_name):
         self._filename = file_name
-        self._text = self.readFile()
-
-    def readFile(self):
-        fileInput = open(self._filename, 'r')
-        text = fileInput.read()
-        return text
 
     def countWords(self):
+        f = open(self._filename, 'r')
         frequencies = dict()
-        for word in self._text:
-            frequency = frequencies.get(word, None)
-            if frequency == None:
-                frequencies[word] = 1
-            else:
-                frequencies[word] = frequency + 1
+        for line in f:
+            for word in line.split():
+                frequency = frequencies.get(word, None)
+                if frequency == None:
+                    frequencies[word] = 1
+                else:
+                    frequencies[word] = frequency + 1
+        
         return frequencies
