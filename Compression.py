5e4bba574cf5cafe8e92613123dd0a0d79ff56b1
aDominguezM <magnotec28@gmail.com>
2015-10-25 20:39:06 -0600

    Compresion

---
+++ Compression.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python2
+#@author: adominguez
+#@date: 20151025 17:10:42
+from itertools import groupby
+import itertools
+
+#Compresion BWT
+def bwt(word):
+	assert "\0" not in word, "Input string cannot contain null character ('\\0')"
+	word += "\0"  
+	table = sorted(word[i:] + word[:i] for i in range(len(word)))    
+	last_column = [row[-1:] for row in table] 
+	return "".join(last_column) 
+
+
+#Compresion RLE
+def rle(s):
+    count = 1
+    prev = ''
+    lst = []
+    strCount = ''
+    for character in s:
+        if character != prev:
+            if prev:
+                entry = (prev,count)
+                lst.append(entry)
+                #print lst
+
+            count = 1
+            prev = character
+        else:
+            count += 1
+    else:
+        entry = (count,character)
+        lst.append(entry)
+        word = ''.join(itertools.imap(str, itertools.chain(*lst)))
+    return word
+
+
+if __name__ == '__main__':
+	
+	s = "^BANANA"
+	print bwt(s)
+	print rle(bwt(s))
