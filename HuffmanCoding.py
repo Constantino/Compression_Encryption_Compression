ea789dc32871ea5a8e1b0e54f16a29e62a502b6b
VictorRiosM <victor.riosm@live.com>
2015-11-13 14:42:39 -0600

    Huffman tree creation

--- HuffmanCoding.py
+++ HuffmanCoding.py
@@ -3,8 +3,8 @@ from collections import Counter, OrderedDict
 
 class Node():
 
-	def __init__(self, item, frequency):
-		self._item = item
+	def __init__(self, name, frequency):
+		self._name = name
 		self._frequency = frequency
 
 	def children(self, left_node, right_node):
@@ -15,8 +15,17 @@ class Node():
 		return cmp(self._frequency, frequency)
 
 	def __repr__(self):
-		return "%s-%s" % (self._item, self._frequency)
+		return "Key: %s Value: %s" % (self._name, self._frequency)
 
 def create_tree(string):
 	nodes = [Node(e, f) for e, f in Counter(string).items()]
+	heapq.heapify(nodes)
+	while len(nodes)>1:
+		left_node = heapq.heappop(nodes)
+		right_node = heapq.heappop(nodes)
+		new_node = Node(left_node._name+right_node._name, left_node._frequency+right_node._frequency)		
+		new_node.children(left_node, right_node)
+		print new_node
+		heapq.heappush(nodes, new_node)
 	return nodes
+
