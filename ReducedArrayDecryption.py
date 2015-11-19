c0e8edaf78d15179bc506ec8b95b3e9b2a8dd8c1
Constantino Mora <constantinomora@gmail.com>
2015-11-18 21:52:24 -0600

    Decryption for all elements

--- ReducedArrayDecryption.py
+++ ReducedArrayDecryption.py
@@ -13,38 +13,38 @@ class ReducedArrayDecryption:
 
 		len_ta_key = len(self._ta_key)
 
-		#for i in range(len_data):
-		flag = 1
-		EST = 0
-		
-		s1,s2,s3 = 0,0,0
-		print self._ta_key
-		while flag:
-			
-			t = [0,0,0]
-			t[0] = self._ta_key[s1]
-			t[1] = self._ta_key[s2]
-			t[2] = self._ta_key[s3]
+		for i in range(len_data):
+			flag = 1
 			EST = 0
-			for k in range(3):
-				EST += ord(t[k]) * self._primary_key[k]
-			print "--- comb: ", t[0]," ", t[1]," ", t[2]
-			print "*** est: ",EST," - ",self._data[0]
-			if EST == self._data[0]:
-				print "equal, data found"
-				flag = 0
-			else:
-				s1 += 1
-				if s1 >= len_ta_key:
-					s2 += 1
-					s1 = 0	
-				if s2 >= len_ta_key:
-					s3 += 1
-					#s1 = 0
-					s2 = 0
-				if s3 >= len_ta_key:
-					#s1 = 1
-					#s2 = 1
-					s3 = 0
+			
+			s1,s2,s3 = 0,0,0
+			
+			while flag:
+				
+				t = [0,0,0]
+				t[0] = self._ta_key[s1]
+				t[1] = self._ta_key[s2]
+				t[2] = self._ta_key[s3]
+				EST = 0
+				for k in range(3):
+					EST += ord(t[k]) * self._primary_key[k]
+				
+				if EST == self._data[i]:
+					flag = 0
+
+				else:
+					s1 += 1
+					if s1 >= len_ta_key:
+						s2 += 1
+						s1 = 0	
+
+					if s2 >= len_ta_key:
+						s3 += 1
+						s2 = 0
+
+					if s3 >= len_ta_key:
+						s3 = 0
+
+			original_data.append( t )
 
 		return original_data
