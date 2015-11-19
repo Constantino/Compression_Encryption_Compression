21f1fa519eb40ac58ba5a703d1d3d591f80c08fb
Constantino Mora <constantinomora@gmail.com>
2015-11-18 22:53:18 -0600

    Added 0s to the right to make the string match by units of 3 characters

--- ReducedArrayEncryption.py
+++ ReducedArrayEncryption.py
@@ -5,16 +5,17 @@ class ReducedArrayEncryption:
 		self._ta_key = []
 		self._rle_output = rle_output
 		self._primary_key = primary_key
+		self._len_rle_output = len(rle_output)
 
 	def encrypt(self):
 
 		self.fill_ta_key(self._rle_output)
+		self.pre_process_rle()
 
 		data_encrypted = []
 		i = 0
-		len_rle_output = len(self._rle_output)
 		
-		while i < len_rle_output:
+		while i < self._len_rle_output:
 			s = 0
 			for j in range(3):
 				s += ord(self._rle_output[i+j])*self._primary_key[j]
@@ -23,6 +24,10 @@ class ReducedArrayEncryption:
 
 		return self._ta_key, data_encrypted
 	
+	def pre_process_rle(self):
+		f = self._len_rle_output - self._len_rle_output%3 + 3
+		self._rle_output = self._rle_output.ljust(f,'0')
+
 	def fill_ta_key(self,rle_output):
 		for e in self._rle_output:
 			if e not in self._ta_key:
