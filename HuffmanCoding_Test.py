411132c444b5230d2a52bdff182b1d59a1c8e4e2
VictorRiosM <victor.riosm@live.com>
2015-11-22 21:34:01 -0600

    Huffman testing

---
+++ HuffmanCoding_Test.py
@@ -0,0 +1,16 @@
+from HuffmanCoding import encode as huffman_encode
+from HuffmanCoding import decode as huffman_decode
+
+
+text = "asdffdll"
+
+# Huffman coding
+huffman_encoded_text, huffman_root = huffman_encode(text) # The root will be necessary to decode
+print "Huffman", huffman_encoded_text
+
+# Huffman decoding
+huffman_decoded_text = huffman_decode(huffman_encoded_text, huffman_root)
+print "Huffman decoded", huffman_decoded_text
+
+
+
