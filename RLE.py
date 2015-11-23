c5f7e6a4c23dbfee1d70edcf0215b81b7519f8a2
Constantino Mora <constantinomora@gmail.com>
2015-11-22 20:31:07 -0600

    Merge for something

--- RLE.py
+++ RLE.py
@@ -8,15 +8,16 @@ def rle(string):
 		while index+1 < len(string) and string[index] == string[index+1]:
 			counter += 1
 			index += 1
-		rle_string += str(counter)
+		rle_string += chr(205) + str(counter) + chr(205)
 		rle_string += string[index]
 		index += 1
 	return rle_string
 
 def invert_rle(string):
 	inv_rle = ''
-	pattern = re.compile("[0-9]+|\\p{ASCII}")
+	regex = chr(205) + "[0-9]+" + chr(205)
+	pattern = re.compile(regex)
 	for g in re.finditer(pattern, string):
-		inv_rle += string[g.end()] * int(g.group())
+		inv_rle += string[g.end()] * int(g.group().lstrip(chr(205)).rstrip(chr(205)))
 	return inv_rle
 
