3928b10657bc8ca8c67f77e3822d9621021a01c7
Constantino Mora <constantinomora@gmail.com>
2015-11-24 20:28:52 -0600

    character as EOF added

--- BWT.py
+++ BWT.py
@@ -1,18 +1,42 @@
+def rotate(string):
+
+        rotations = []
+
+        len_string = len(string)
+        new_string = string
+        rotations.append(string)
+        for i in range(len_string-1,0,-1):
+            new_string = new_string[len_string-1]+new_string[:len_string-1]
+            rotations.append(new_string)
+
+        rotations.sort()
+
+        return rotations
+
+def get_BWT(string):
+
+    rotations = rotate(string)
+    new_string = ""
+
+    for e in rotations:
+        new_string += e[-1]
+
+    return new_string
+
 def BWT(string):
-    table = [string[n:] + string[:n] for n in xrange(len(string))]
-    table = sorted(table)
-    return "".join([rotation[-1] for rotation in table])
-
-def invert_BWT(string):
-    string_list = [e for e in string]
-    sorted_list = sorted(string_list)
-    len_sorted_list = len(sorted_list)
     
-    for j in range(len_sorted_list-1):
-        temp = []
-        for i in range(len(sorted_list)):
-            temp.append(string_list[i]+sorted_list[i])
+    assert "\0" not in string, "Input string cannot contain null character ('\\0')"
+    string += "\0"  # Add end of file marker
 
-        sorted_list = sorted(temp)
+    return get_BWT(string)
+
+
+def invert_BWT(r):
     
-    return sorted_list[-1]
+    table = [""] * len(r)  
+    for i in range(len(r)):
+        table = sorted(r[i] + table[i] for i in range(len(r)))  
+    s = [row for row in table if row.endswith("\0")][0]  
+    return s.rstrip("\0")  
+
+
