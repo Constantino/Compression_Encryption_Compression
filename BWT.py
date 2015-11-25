bfca4d0dee64fdd02f51bcfcabb3ffa7b1c78120
Constantino Mora <constantinomora@gmail.com>
2015-11-25 00:28:05 -0600

    Algorithm repaired

--- BWT.py
+++ BWT.py
@@ -1,42 +1,23 @@
-def rotate(string):
-
-        rotations = []
-
-        len_string = len(string)
-        new_string = string
-        rotations.append(string)
-        for i in range(len_string-1,0,-1):
-            new_string = new_string[len_string-1]+new_string[:len_string-1]
-            rotations.append(new_string)
-
-        rotations.sort()
-
-        return rotations
-
-def get_BWT(string):
-
-    rotations = rotate(string)
-    new_string = ""
-
-    for e in rotations:
-        new_string += e[-1]
-
-    return new_string
-
 def BWT(string):
     
     assert "\0" not in string, "Input string cannot contain null character ('\\0')"
     string += "\0"  # Add end of file marker
 
-    return get_BWT(string)
+    table = [string[n:] + string[:n] for n in xrange(len(string))]
+    table = sorted(table)
+
+    return "".join([rotation[-1] for rotation in table])
 
 
 def invert_BWT(r):
-    
+
     table = [""] * len(r)  
     for i in range(len(r)):
         table = sorted(r[i] + table[i] for i in range(len(r)))  
     s = [row for row in table if row.endswith("\0")][0]  
     return s.rstrip("\0")  
 
-
+"""
+Based on:
+https://en.wikipedia.org/wiki/Burrows%E2%80%93Wheeler_transform
+"""
